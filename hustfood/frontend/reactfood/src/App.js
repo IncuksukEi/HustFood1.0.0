4168b82b0fdf8364346449c7b5fec087a5da4228
TaDinhThien05 <thien.td235839@sis.hust.edu.vn>
2025-05-01 23:55:48 +0700

    home ver 1.0

--- hustfood/frontend/reactfood/src/App.js
+++ hustfood/frontend/reactfood/src/App.js
@@ -5,6 +5,9 @@ import Product from './pages/Product/Product';
 import Cart from './pages/Cart/Cart';
 import Profile from './pages/Profile/Profile';
 import Pay from './pages/Pay/Pay';
+import Notification from './pages/Notification/Notification';
+import Help from './pages/Help/Help';
+import Management from './pages/Management/Management';
 
 function App() {
   return (
--- hustfood/frontend/reactfood/src/pages/Home/Home.js
+++ hustfood/frontend/reactfood/src/pages/Home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'; // Import các icon cần dùng
 import Header from '../../components/Header/Header'; 
+import Sidebar from '../../components/Sidebar/Sidebar';
 import Footer from '../../components/Footer/Footer';
 import './Home.css';
 import "../../styles/base.css";
@@ -11,36 +14,16 @@ function Home() {
     <div className="app__container">
       <div className="grid">
         <div className="grid__row app__content">
-          <div className="grid__column-2">
-            <nav className="category">
-              <h3 className="category__heading">
-                <i className="category__heading-icon fa-solid fa-list"></i>
-                Danh mục
-              </h3>
-              <ul className="category-list">
-                <li className="category-item category-item--active">
-                  <a href="#" className="category-item__link">Bim Bim</a>
-                </li>
-                <li className="category-item">
-                  <a href="#" className="category-item__link">Nước ngọt</a>
-                </li>
-                <li className="category-item">
-                  <a href="#" className="category-item__link">Bánh kẹo</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <Sidebar />
           <div className="grid__column-10">
             <div className="home-filter">
               <span className="home-filter__label">Sắp xếp theo</span>
               <button className="home-filter__btn btn">Phổ biến</button>
               <button className="home-filter__btn btn btn--primary">Mới nhất</button>
-              {/* cái nào khi ấn chọn thì thêm btn--primary */}
               <button className="home-filter__btn btn">Bán chạy</button>
               <div className="select-input">
                 <span className="select-input__label">Giá</span>
-                <i className="select-input__icon fa-solid fa-angle-down"></i>
-                {/* List options */}
+                <FontAwesomeIcon icon={faAngleDown} className="select-input__icon" /> {/* Sử dụng icon từ react-fontawesome */}
                 <ul className="select-input__list">
                   <li className="select-input__item">
                     <a href="#" className="select-input__link">Giá: Thấp đến cao</a>
@@ -53,25 +36,25 @@ function Home() {
               <div className="home-filter__page">
                 <span className="home-filter__page-num">
                   <span className="home-filter__page-current">1</span> / <span className="home-filter__page-all">14</span>
-                  <div className="home-filter__page-control">
-                    <a href="#" className="home-filter__page-btn home-filter__page-btn--disabled">
-                      <i className="home-filter__page-icon fa-solid fa-angle-left"></i>
-                    </a>
-                    <a href="#" className="home-filter__page-btn">
-                      <i className="home-filter__page-icon fa-solid fa-angle-right"></i>
-                    </a>
-                  </div>
                 </span>
+                <div className="home-filter__page-control">
+                  <a href="#" className="home-filter__page-btn home-filter__page-btn--disabled">
+                    <FontAwesomeIcon icon={faAngleLeft} className="home-filter__page-icon" /> {/* Sử dụng icon từ react-fontawesome */}
+                  </a>
+                  <a href="#" className="home-filter__page-btn">
+                    <FontAwesomeIcon icon={faAngleRight} className="home-filter__page-icon" /> {/* Sử dụng icon từ react-fontawesome */}
+                  </a>
+                </div>
               </div>
             </div>
             <div className="home-product">
               <ProductList />
-          /*code ở đây thiếu*/
+              {/* code ở đây thiếu */}
             </div>
             <ul className="pagination home-product__pagination">
               <li className="pagination-item">
                 <a href="#" className="pagination-item__link">
-                  <i className="pagination-item__icon fa-solid fa-angle-left"></i>
+                  <FontAwesomeIcon icon={faAngleLeft} className="pagination-item__icon" /> {/* Sử dụng icon từ react-fontawesome */}
                 </a>
               </li>
               <li className="pagination-item pagination-item--active">
@@ -91,7 +74,7 @@ function Home() {
               </li>
               <li className="pagination-item">
                 <a href="#" className="pagination-item__link">
-                  <i className="pagination-item__icon fa-solid fa-angle-right"></i>
+                  <FontAwesomeIcon icon={faAngleRight} className="pagination-item__icon" /> {/* Sử dụng icon từ react-fontawesome */}
                 </a>
               </li>
             </ul>
