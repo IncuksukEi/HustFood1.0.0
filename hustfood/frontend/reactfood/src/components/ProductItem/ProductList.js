541d7683a7a5888b1eb0e4687b781ab7eb43a198
TaDinhThien05 <thien.td235839@sis.hust.edu.vn>
2025-05-01 23:55:48 +0700

    home ver 1.0

--- hustfood/frontend/reactfood/src/components/ProductItem/ProductList.js
+++ hustfood/frontend/reactfood/src/components/ProductItem/ProductList.js
@@ -1,8 +1,25 @@
-import React from "react";
-import ProductItem from "../components/ProductItem";
-import products from "../data/products";
+import React, {useState, useEffect} from "react";
+import ProductItem from "./ProductItem";
+//import products from "../data/products";
+import "./ProductList.css"; // Import your CSS file for styling
+import {getProducts} from "../../services/productService"; // Import the product service
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="grid__row">
       {products.map((product, index) => (
