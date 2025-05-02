6556ebc25f754816fd8eb631090f66605320e070
TaDinhThien05 <thien.td235839@sis.hust.edu.vn>
2025-05-02 16:06:21 +0700

    home -> homesearch, + new home

--- hustfood/frontend/reactfood/src/App.js
+++ hustfood/frontend/reactfood/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import HomeSearch from './pages/HomeSearch/HomeSearch';
 import Product from './pages/Product/Product';
 import Cart from './pages/Cart/Cart';
 import Profile from './pages/Profile/Profile';
@@ -8,12 +8,14 @@ import Pay from './pages/Pay/Pay';
 import Notification from './pages/Notification/Notification';
 import Help from './pages/Help/Help';
 import Management from './pages/Management/Management';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Search" element={<HomeSearch />} />
         <Route path="/notifications" element={<Notification />}/>
         <Route path="/help" element={<Help />} />
         <Route path="/management" element={<Management />} />
--- hustfood/frontend/reactfood/src/pages/Home/Home.js
+++ hustfood/frontend/reactfood/src/pages/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'; // Import các icon cần dùng
 import Header from '../../components/Header/Header'; 
-import Sidebar from '../../components/Sidebar/Sidebar';
 import Footer from '../../components/Footer/Footer';
 import './Home.css';
 import "../../styles/base.css";
-import ProductList from '../../components/ProductItem/ProductList';
+import BillBoard from '../../components/Billboard/BillBoard';
+import Category from '../../components/Category/Category';
 
 function Home() {
   return (
@@ -14,71 +12,8 @@ function Home() {
     <div className="app__container">
       <div className="grid">
         <div className="grid__row app__content">
-          <Sidebar />
-          <div className="grid__column-10">
-            <div className="home-filter">
-              <span className="home-filter__label">Sắp xếp theo</span>
-              <button className="home-filter__btn btn">Phổ biến</button>
-              <button className="home-filter__btn btn btn--primary">Mới nhất</button>
-              <button className="home-filter__btn btn">Bán chạy</button>
-              <div className="select-input">
-                <span className="select-input__label">Giá</span>
-                <FontAwesomeIcon icon={faAngleDown} className="select-input__icon" /> {/* Sử dụng icon từ react-fontawesome */}
-                <ul className="select-input__list">
-                  <li className="select-input__item">
-                    <a href="#" className="select-input__link">Giá: Thấp đến cao</a>
-                  </li>
-                  <li className="select-input__item">
-                    <a href="#" className="select-input__link">Giá: Cao đến thấp</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="home-filter__page">
-                <span className="home-filter__page-num">
-                  <span className="home-filter__page-current">1</span> / <span className="home-filter__page-all">14</span>
-                </span>
-                <div className="home-filter__page-control">
-                  <a href="#" className="home-filter__page-btn home-filter__page-btn--disabled">
-                    <FontAwesomeIcon icon={faAngleLeft} className="home-filter__page-icon" /> {/* Sử dụng icon từ react-fontawesome */}
-                  </a>
-                  <a href="#" className="home-filter__page-btn">
-                    <FontAwesomeIcon icon={faAngleRight} className="home-filter__page-icon" /> {/* Sử dụng icon từ react-fontawesome */}
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="home-product">
-              <ProductList />
-              {/* code ở đây thiếu */}
-            </div>
-            <ul className="pagination home-product__pagination">
-              <li className="pagination-item">
-                <a href="#" className="pagination-item__link">
-                  <FontAwesomeIcon icon={faAngleLeft} className="pagination-item__icon" /> {/* Sử dụng icon từ react-fontawesome */}
-                </a>
-              </li>
-              <li className="pagination-item pagination-item--active">
-                <a href="#" className="pagination-item__link">1</a>
-              </li>
-              <li className="pagination-item">
-                <a href="#" className="pagination-item__link">2</a>
-              </li>
-              <li className="pagination-item">
-                <a href="#" className="pagination-item__link">3</a>
-              </li>
-              <li className="pagination-item">
-                <a href="#" className="pagination-item__link">...</a>
-              </li>
-              <li className="pagination-item">
-                <a href="#" className="pagination-item__link">14</a>
-              </li>
-              <li className="pagination-item">
-                <a href="#" className="pagination-item__link">
-                  <FontAwesomeIcon icon={faAngleRight} className="pagination-item__icon" /> {/* Sử dụng icon từ react-fontawesome */}
-                </a>
-              </li>
-            </ul>
-          </div>
+          <BillBoard />
+          <Category />
         </div>
       </div>
     </div>
