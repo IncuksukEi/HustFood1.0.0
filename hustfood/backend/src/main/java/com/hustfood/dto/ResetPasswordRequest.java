8411bb74ecd8fd46dac0474cb7907994382d2aa1
IncuksukEi <incuksuk123@gmail.com>
2025-05-13 22:06:39 +0700

    fix DTO resetpassword

--- hustfood/backend/src/main/java/com/hustfood/dto/ResetPasswordRequest.java
+++ hustfood/backend/src/main/java/com/hustfood/dto/ResetPasswordRequest.java
@@ -2,11 +2,16 @@ package com.hustfood.dto;
 
 public class ResetPasswordRequest {
 
+    private String oldPassword;
     private String newPassword;
 
     // Getters và Setters
+    public String getOldPassword() {
+        return oldPassword;
     }
 
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
     }
 
     public String getNewPassword() {
