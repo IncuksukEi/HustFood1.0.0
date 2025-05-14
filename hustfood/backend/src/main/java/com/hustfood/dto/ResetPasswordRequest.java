055deb57008e05d80a7a9b35f179f5a7e8ed085f
TaDinhThien05 <thien.td235839@sis.hust.edu.vn>
2025-05-14 13:59:28 +0700

    fix resetpassword conflig

--- hustfood/backend/src/main/java/com/hustfood/dto/ResetPasswordRequest.java
+++ hustfood/backend/src/main/java/com/hustfood/dto/ResetPasswordRequest.java
@@ -22,28 +22,3 @@ public class ResetPasswordRequest {
         this.newPassword = newPassword;
     }
 }
-
-package com.hustfood.dto;
-
-public class ResetPasswordRequest {
-
-    private String password;
-    private String newPassword;
-
-    // Getters và Setters
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
-}
