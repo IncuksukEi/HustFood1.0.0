1fe906da9cc05c67f79c0814535c111eb2ef646a
IncuksukEi <incuksuk123@gmail.com>
2025-05-15 23:31:02 +0700

    fix timeOrder

--- hustfood/backend/src/main/java/com/hustfood/dto/OrderResponseDTO.java
+++ hustfood/backend/src/main/java/com/hustfood/dto/OrderResponseDTO.java
@@ -1,5 +1,6 @@
 package com.hustfood.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ public class OrderResponseDTO {
     private Long orderId;
     private BigDecimal totalPrice;
     private String orderAddress;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime orderTime;
     private String status;
     private List<OrderDetailResponseDTO> products;
