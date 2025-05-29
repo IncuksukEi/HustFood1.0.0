001e78423f91ff4a2f59e429df766e368ddba7ac
IncuksukEi <incuksuk123@gmail.com>
2025-05-29 14:58:55 +0700

    fix AdminOrders

--- hustfood/backend/src/main/java/com/hustfood/entity/Product.java
+++ hustfood/backend/src/main/java/com/hustfood/entity/Product.java
@@ -51,7 +51,4 @@ public class Product {
     @JsonIgnore  
     private List<OrderDetail> orderDetails;
 
-    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
-    @JsonIgnore
-    private List<Inventory> inventories;
 }
